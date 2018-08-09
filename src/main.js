29c9e3d96385ca48fb737130781733a7ee64405d
tjmonsi <tjmonsi@gmail.com>
2018-08-09 02:22:58 -0400

    Added Polymer

--- src/main.js
+++ src/main.js
@@ -1,40 +1,33 @@
+import { PolymerElement, html } from '../node_modules/@polymer/polymer/polymer-element.js';
+
 const form = document.querySelector('#form');
-const helloWorld = document.querySelector('hello-world');
-form.addEventListener('submit', e => {
-  const { target } = e;
-  e.preventDefault();
-  helloWorld.name = target.name.value;
+const helloWorld = document.querySelector('#hello');
+form.addEventListener('submit', event => {  
+  event.preventDefault();
+  helloWorld.name = form.variableone.value;
 });
 
-class HelloWorld extends HTMLElement {
-  constructor () {
-    super();
-    this.attachShadow({ mode: 'open' });
-    this._name = '';
-    this._render();    
+class HelloWorld extends PolymerElement {
+  static get properties () {
+    return {
+      name: {
+        type: String
+      }
+    }
   }
   
-  set name (name) {
-    this._name = name;
-    this._render();
-  }
-  
-  get name () {
-    return this._name;
-  }
-  
-  _render () {
-    this.shadowRoot.innerHTML = `
+  static get template () {
+    return html`
       <style>
         span {
           color: blue;
         }
       </style>
       <span>
-        Hello ${this.name}
+        Hello [[name]]
       </span>
       <slot></slot>
-    `;
+    `
   }
 }
 
